3c4c285e4851e5ad3db9f34fd0bfb44a41427a87
Rohit72099 <rk464152@gmail.com>
2025-11-02 05:58:55 +0530

    d

--- infra/nagios/install-nagios.sh
+++ infra/nagios/install-nagios.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
-sudo yum install httpd php gcc glibc glibc-common gd gd-devel make net-snmp openssl-devel unzip -y
+
+# Update package lists
+sudo apt-get update
+
+# Install required packages
+sudo apt-get install -y apache2 php libapache2-mod-php php-gd unzip wget build-essential openssl libssl-dev apache2-utils
+
+# Create Nagios user and group
 sudo useradd nagios
-sudo passwd nagios
-sudo usermod -aG apache nagios
-echo "Nagios installed. Configure eerieverse.cfg next."
+sudo usermod -a -G nagios www-data
+
+# Download and install Nagios Core
+cd /tmp
+wget https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.6.tar.gz
+tar xzf nagios-4.4.6.tar.gz
+cd nagioscore-nagios-4.4.6
+sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
+sudo make all
+sudo make install
+sudo make install-daemoninit
+sudo make install-commandmode
+sudo make install-config
+sudo make install-webconf
+
+# Download and install Nagios Plugins
+cd /tmp
+wget https://github.com/nagios-plugins/nagios-plugins/releases/download/release-2.3.3/nagios-plugins-2.3.3.tar.gz
+tar xzf nagios-plugins-2.3.3.tar.gz
+cd nagios-plugins-2.3.3
+sudo ./tools/setup
+sudo ./configure
+sudo make
+sudo make install
+
+# Configure Apache
+sudo a2enmod rewrite
+sudo a2enmod cgi
+
+# Create nagiosadmin user
+sudo htpasswd -c /usr/local/nagios/etc/htpasswd.users nagiosadmin
+
+# Start services
+sudo systemctl restart apache2
+sudo systemctl start nagios
+sudo systemctl enable nagios
+
+echo "Nagios installation completed. Access the web interface at http://your-ip/nagios"
--- server/config/database.js
+++ server/config/database.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        // Remove deprecated options
         const connectionParams = {
-            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
-            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            serverApi: {
+                version: '1',
+                strict: true,
+                deprecationErrors: true,
+            },
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
         };
 
         console.log('Attempting to connect to MongoDB...');
